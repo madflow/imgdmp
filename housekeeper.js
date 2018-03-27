a5fcedeb4b9172bbd62a7a3785b1449b54b1f8cd
madflow <madflow@web.de>
2018-03-27 12:28:47 +0200

    housekeeper cron worker

---
+++ housekeeper.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function deleteExpired(db) {
+  db.find(
+    {
+      $where: function() {
+        return this.expires !== null && this.expires < new Date();
+      }
+    },
+    function(err, docs) {
+      if (err) {
+        throw err;
+      }
+      docs.forEach(image => {
+        fs.unlink(image.path, function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`DELETED FILE ${image.filename}`);
+          }
+          db.remove({ _id: image._id }, {}, function(err, numRemoved) {
+            if (err) {
+              console.log(err);
+            }
+            console.log(`DELETED DB ENTRY`);
+          });
+        });
+      });
+    }
+  );
+}
+
+module.exports = {
+  deleteExpired: deleteExpired
+};
